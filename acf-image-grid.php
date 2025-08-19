4e4036714e2fd0597590319a38cbfd92381a9b51
lancemonotone <lancemonotone@gmail.com>
2025-08-18 20:48:13 -0400

    chore(acf-image-grid): switch to MU scaffold boot and define PLUGIN_* constants

--- acf-image-grid.php
+++ acf-image-grid.php
@@ -17,10 +17,28 @@ $config_file = plugin_dir_path(__FILE__) . 'config.php';
 if (file_exists($config_file)) {
     $plugin_config = include $config_file;
     if ($plugin_config) {
-        define('PLUGIN_CONFIG', $plugin_config);
-        define('PLUGIN_SLUG', $plugin_config['slug']);
-        define('PLUGIN_TEXT_DOMAIN', $plugin_config['text_domain']);
+        if (!defined('PLUGIN_CONFIG')) {
+            define('PLUGIN_CONFIG', $plugin_config);
+        }
+        if (!defined('PLUGIN_SLUG')) {
+            define('PLUGIN_SLUG', $plugin_config['slug']);
+        }
+        if (!defined('PLUGIN_TEXT_DOMAIN')) {
+            define('PLUGIN_TEXT_DOMAIN', $plugin_config['text_domain']);
+        }
     }
 }
 
-require_once plugin_dir_path(__FILE__) . 'classes/class.autoloader.php';
+if (!defined('PLUGIN_BASE_PATH')) {
+    define('PLUGIN_BASE_PATH', plugin_dir_path(__FILE__));
+}
+if (!defined('PLUGIN_BASE_URL')) {
+    define('PLUGIN_BASE_URL', plugin_dir_url(__FILE__));
+}
+
+// Use the MU scaffold
+if (function_exists('monotone_acf_scaffold_boot')) {
+    monotone_acf_scaffold_boot();
+} else {
+    error_log('MU scaffold not found: Please ensure wp-content/mu-plugins/monotone-acf-scaffold.php is installed.');
+}
